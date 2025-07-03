9add876b522b83ab2661a0bd7ecf8e1ed3fb5063
Lucaaprileee <Lucaaprile2@gmail.com>
2025-07-03 16:54:28 +0200

    Update AppleDevice
    -add appledevice

--- NotificheSmartHome/src/AppleDevice.java
+++ NotificheSmartHome/src/AppleDevice.java
@@ -1,6 +1,6 @@
 public interface AppleDevice {
-    void addObserver(Observer o);
-    void removeObserver(Observer o);
+    void addObserver(Observer observer);
+    void removeObserver(Observer observer);
     void notifyObserver(String message);
     void changeState(String newState);
 }
---
+++ NotificheSmartHome/src/AppleSensor.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleSensor implements AppleDevice{
+
+    private List<Observer> observers = new ArrayList<>();
+    private String state;
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObserver(String message) {
+        for (Observer o : observers) {
+            o.update("AppleSensor: " + message);
+        }
+    }
+
+    @Override
+    public void changeState(String newState) {
+        this.state = newState;
+        notifyObserver("Stato cambaito a " + newState);
+    }
+
+}
